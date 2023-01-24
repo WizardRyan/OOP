0313e361ab99fdfce975bf793ba7d88aad65b0a8
WizardRyan <ryan.andersen117@gmail.com>
2023-01-23 17:00:01 -0700

    added file parsing for ellipses, added immediate output

--- StrategyPatternWithShapes/ShapesApp/ShapesAppDriver.cs
+++ StrategyPatternWithShapes/ShapesApp/ShapesAppDriver.cs
@@ -9,6 +9,8 @@ using System.IO;
 using System.Text.Json.Serialization;
 using System.Xml;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Runtime.ConstrainedExecution;
 
 namespace ShapesApp
 {
@@ -16,33 +18,99 @@ namespace ShapesApp
     {
         static void Main(string[] args)
         {
-            var dataFromFile = ReadInFile();
-
-            foreach (var item in dataFromFile)
+            try
             {
-                Console.WriteLine(item);
+                var dataFromFile = ReadInFile();
+                var areaTotals = CalculateAreaTotals(dataFromFile);
+                HandleOutput(areaTotals);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
             }
 
+            //make the console hang
             Console.ReadKey();
         }
 
-
-        static Dictionary<string, object> ReadJson(string json)
+        static void HandleOutput(Dictionary<string, double> areaTotals)
         {
-            return System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, object>>(json);
+            Console.WriteLine("Would you like to (1) view output immediately, or  (2) save to a CSV? (enter num): ");
+            string response = Console.ReadLine();
+            if(response == "1")
+            {
+                DisplayOutput(areaTotals);
+            }
         }
 
-        static Dictionary<string, object> ReadInFile()
+        static void DisplayOutput(Dictionary<string, double> areaTotals)
+        {
+            Console.WriteLine("\n{0,-40} {1,5:N2}\n", "Total area for all shapes: ", areaTotals["Total for All Shapes"]);
+            Console.WriteLine("{0,-40} {1,5:N2}", "    Ellipses: ", areaTotals["Ellipses"]);
+            Console.WriteLine("{0,-40} {1,5:N2}", "        Non-circle Ellipses: ", areaTotals["Non-CircleEllipses"]);
+            Console.WriteLine("{0,-40} {1,5:N2}", "        Circles: ", areaTotals["Circles"]);
+
+        }
+
+        static Dictionary<string, double> CalculateAreaTotals(dynamic dataFromFile)
+        {
+
+            double ellipseAreaTotals = 0;
+            double circleAreaTotals = 0;
+            double nonCircleEllipseAreaTotals = 0;
+            double totalAreaAllShapes = 0;
+
+            double area = 0;
+
+            foreach (dynamic item in dataFromFile)
+            {
+                string shapeName = Convert.ToString(item.shapeName).ToLower();
+
+                switch(shapeName)
+                {
+                    case "ellipse":
+                        var ellipse = new Ellipse(Convert.ToDouble(item.majorAxis), Convert.ToDouble(item.minorAxis));
+                        area = ellipse.GetArea();
+                        ellipseAreaTotals += area;
+                        nonCircleEllipseAreaTotals += area;
+                        totalAreaAllShapes += area;
+                        break;
+
+                    case "circle":
+                        var circle = new Circle(Convert.ToDouble(item.radius));
+                        area = circle.GetArea();
+                        ellipseAreaTotals+= area;
+                        circleAreaTotals += area;
+                        totalAreaAllShapes+= area;
+                        break;
+                }
+            }
+
+
+            return new Dictionary<string, double>() {
+                {"Ellipses", ellipseAreaTotals },
+                {"Circles", circleAreaTotals},
+                {"Non-CircleEllipses", nonCircleEllipseAreaTotals},
+                { "Total for All Shapes", totalAreaAllShapes}
+            };
+        }
+
+        static dynamic ReadJson(string json)
+        {
+            return JArray.Parse(json);
+        }
+
+        static dynamic ReadInFile()
         {
             bool validExt = false;
             string fileName = "";
             string fileExt = "";
 
-            Dictionary<string, object> fileData = new Dictionary<string, object>();
+            dynamic fileData = null;
 
             while (!validExt)
             {
-                Console.WriteLine("Enter File Name: ");
+                Console.WriteLine("Enter file name (must be of type XML or JSON): ");
                 fileName = Console.ReadLine();
 
                 fileExt = System.IO.Path.GetExtension(fileName).ToLower();
